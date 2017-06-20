0ea9c058990045cb88c4db1d78f898bfff4b1788
Sam Hernández <sam@mispistachos.cl>
2017-06-19 21:16:38 -0400

    add: tele-bot

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,27 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
+    import sys
+    import time
+    import telepot
+    from telepot.loop import MessageLoop
+
+    def handle(msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        print(content_type, chat_type, chat_id)
+
+        if content_type == 'text':
+            bot.sendMessage(chat_id, msg['text'])
+
+    TOKEN = '391521158:AAFilAONR14D5N1CJrguMvbakVhbY7nHUdo'
+
+    bot = telepot.Bot(TOKEN)
+    MessageLoop(bot, handle).run_as_thread()
+    print ('Listening ...')
+
+    # Keep the program running.
+    # while 1:
+    #     time.sleep(10)
     return render(request, 'index.html')
 
 
