eba7a9740cadd5e9ecc346d22135a54662fa5254
sfehlandt <sebafehlandt@gmail.com>
2017-06-21 00:01:06 -0400

    Add Pet model. References #6

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 
 # Create your models here.
+NAME_VERBOSE      = 'Nombre'
+OWNER_VERBOSE     = 'Dueño'
+CREDITS_VERBOSE   = 'Créditos'
+STATUS_VERBOSE    = 'Estado'
+HAPPINESS_VERBOSE = 'Felicidaad'
+
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Pet(models.Model):
+
+    name = models.CharField(max_length=30, verbose_name=NAME_VERBOSE)
+
+    owner = models.CharField(max_length=15, verbose_name=OWNER_VERBOSE)
+
+    credits = models.IntegerField(default=0, verbose_name=CREDITS_VERBOSE)
+
+    status = models.IntegerField(default=0, verbose_name=STATUS_VERBOSE)
+
+    happiness = models.IntegerField(default=0, verbose_name=HAPPINESS_VERBOSE)
--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Pet
 
 # Create your tests here.
+class PetModelTestCase(TestCase):
+
+    def setUp(self):
+        self.pet_data = {
+            'name'     : 'My Kitten',
+            'owner'    : 'Owned',
+            'credits'  : 300,
+            'status'   : 0,
+            'happiness': 0
+        }
+
+    def test_model_create_pet(self):
+        self.old_count = Pet.objects.count()
+        self.pet = Pet(**self.pet_data)
+        self.pet.save()
+        self.new_count = Pet.objects.count()
+        self.assertEquals(self.old_count + 1, self.new_count)
+
+    def test_model_delete_pet(self):
+        self.pet = Pet(**self.pet_data)
+        self.pet.save()
+        self.old_count = Pet.objects.count()
+        Pet.objects.filter(pk=self.pet.pk).delete()
+        self.new_count = Pet.objects.count()
+        self.assertEquals(self.old_count - 1, self.new_count)
